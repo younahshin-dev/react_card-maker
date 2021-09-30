0368b904c15d3b82faa66d2a527d6d7b667f1d81
younahshin-dev <roaving@gmail.com>
2021-09-30 11:36:29 +0900

    add firebase AuthProvider

---
+++ src/app.jsx
@@ -0,0 +1,27 @@
+
+import './app.css';
+
+function App() {
+  return (
+    
+      <div class="loginForm">
+        <header class="loginHeader">
+          <img id="imglogo" src="/images/logo.png" alt=""/>
+          <span>Business Card Maker</span>
+        </header>
+        <div class="loginBody">
+          <span>Login</span>
+          <div class="LoginLinks">
+            <span>Google</span>
+            <span>Github</span>
+          </div>
+        </div>
+        <footer>
+          Code your dream
+        </footer>
+      </div>
+    
+    );
+}
+
+export default App;
---
+++ src/service/firebase.js
@@ -0,0 +1,12 @@
+
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_APP_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
