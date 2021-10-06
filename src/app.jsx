aeaad89daef632ff13eeac7b701668f4383638c9
younahshin-dev <roaving@gmail.com>
2021-10-06 17:41:47 +0900

    login UI complete

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,13 @@
-
-import './app.css';
+import React from 'react'
+import styles from './app.module.css';
 import Login from './components/login/login';
 
 function App({authService}) {
-  return <Login authService={authService}/>;
+  return (
+    <div className={styles.app}>
+      <Login authService={authService}/>;
+    </div>
+  )
 }
 
 export default App;
--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,5 +1,10 @@
 import React from 'react';
+import styles from './footer.module.css'
 
-const Footer = props => <h1>Footer</h1>;
+const Footer = props =>
+    <footer className={styles.footer}>
+        <h1 className={styles.copyright}>Code your dream</h1>
+    </footer> 
+    
 
 export default Footer;
